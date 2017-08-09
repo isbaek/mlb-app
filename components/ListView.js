e4e0fbb00834f6f00e409c3e03ad40c3611cc07b
Inseo B <yunibaek@hotmail.com>
2017-08-09 19:19:38 -0400

    List View: display score and winner

--- components/ListView.js
+++ components/ListView.js
@@ -2,6 +2,23 @@ import React from "react";
 
 import fetchGames from "./utils/fetchGames.js";
 
+function GameCardTeam({ team }) {
+  if (team.winner) {
+    return <p><b>{team.name} - {team.score}</b></p>;
+  }
+  return <p>{team.name} - {team.score}</p>;
+}
+
+function GameCard({ game }) {
+  return (
+    <li key={game.id}>
+      <GameCardTeam team={game.home} />
+      <GameCardTeam team={game.away} />
+      <p>{game.status}</p>
+    </li>
+  );
+}
+
 class ListView extends React.Component {
   constructor(props) {
     super(props);
@@ -20,19 +37,14 @@ class ListView extends React.Component {
   }
 
   render() {
+    // wait for the games to load
     if (this.state.isLoading) {
       return <div> Loading</div>;
     }
     return (
       <div>
         <ul>
-          {this.state.games.map(game => (
-            <li key={game.id}>
-              <p>{game.homeName}</p>
-              <p>{game.awayName}</p>
-              <p>{game.status}</p>
-            </li>
-          ))}
+          {this.state.games.map(game => <GameCard game={game} />)}
         </ul>
       </div>
     );
