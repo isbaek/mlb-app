b9c766f088f1b89d9675424a09f67655c990a0fc
Inseo B <yunibaek@hotmail.com>
2017-08-09 18:11:25 -0400

    Fetch Games: Add fetching api for games in list view

---
+++ components/utils/fetchGames.js
@@ -0,0 +1,38 @@
+////
+// FETCH API
+// make a fetch api request to get the mlb game matches on a given day
+////
+
+// extracts and cleans up data
+function normalizeGame(game) {
+  return {
+    id: game.id,
+    homeName: game.home_team_name,
+    awayName: game.away_team_name,
+    status: game.status.status, // final, postponed or cancelled
+    linescore: game.linescore.r // either home or away
+  };
+}
+
+// Fetch all the games in a given day
+function fetchGameOnDate() {
+  return (
+    fetch(
+      "http://gd2.mlb.com/components/game/mlb/year_2014/month_04/day_06/master_scoreboard.json"
+    )
+      // Parse response as JSON
+      .then(res => res.json())
+      // Cleanup big JSON mess into posts
+      .then(payload => {
+        return payload.data.games.game.map(child => normalizeGame(child));
+      })
+      // catch any errors
+      .catch(err => {
+        console.log("parsing failed", err);
+      })
+  );
+}
+
+export default function fetchGames() {
+  return fetchGameOnDate();
+}
