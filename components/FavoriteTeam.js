a1a23df3cb05eef1d982a447b7f7bca16d292859
Inseo B <yunibaek@hotmail.com>
2017-08-10 07:13:41 -0400

    Favorite Team: implement a favorite team bar

--- components/FavoriteTeam.js
+++ components/FavoriteTeam.js
@@ -10,14 +10,36 @@ function teamLogoURL(teamCode) {
   return `https://securea.mlb.com/mlb/images/team_logos/124x150/${teamCode}.png`;
 }
 
+function setFavoriteTeam(teamCode) {
+  localStorage.setItem("fav", teamCode);
+  window.location.reload();
+}
+
+function hasLocalStorage() {
+  try {
+    return localStorage && true;
+  } catch (exception) {
+    return false;
+  }
+}
+
+function getFavoriteTeam() {
+  if (!hasLocalStorage()) {
+    return "tor";
+  }
+  // get fav team, or toronto blue jays by default
+  return localStorage.getItem("fav") || "tor";
+}
+
 export default function FavoriteTeam() {
   return (
     <div className="TeamList">
       {TEAMS.map(team => (
         <img
-          className="TeamImage"
+          className={`TeamImage ${getFavoriteTeam() === team.code ? "active" : ""}`}
           src={teamLogoURL(team.code)}
           alt={team.name}
+          onClick={() => setFavoriteTeam(team.code)}
         />
       ))}
     </div>
--- pages/games.js
+++ pages/games.js
@@ -2,17 +2,19 @@ import moment from "moment";
 
 import Page from "../components/Page";
 import ListView from "../components/ListView";
+import FavoriteTeam from "../components/FavoriteTeam";
 
 // Supose date is formatted as YYYY/MM/DD
 function parseDate(dateStr) {
   if (!dateStr) {
     return moment();
   }
-  return moment(dateStr, 'YYYY/MM/DD');
+  return moment(dateStr, "YYYY/MM/DD");
 }
 
-const Games = (props) => (
+const Games = props => (
   <Page>
+    <FavoriteTeam />
     <ListView date={parseDate(props.url.query.date)} />
   </Page>
 );
