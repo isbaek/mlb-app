66420156420f1fd90c63729442087c530dfde7a1
Inseo B <yunibaek@hotmail.com>
2017-08-10 09:24:41 -0400

    Page/Game: change minor code syntax

--- pages/game.js
+++ pages/game.js
@@ -1,7 +1,7 @@
 import Page from "../components/Page";
 import DetailView from "../components/DetailView";
 
-const Game = (props) => (
+const Game = props => (
   <Page>
     <DetailView gameDataDirectory={props.url.query.id} />
   </Page>
