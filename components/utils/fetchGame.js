ab2d4687e2db92a6906cb3ea022beebbd70e485b
Inseo B <yunibaek@hotmail.com>
2017-08-09 21:29:14 -0400

    fetch Game: add comments, and fetch runs, hits and errors

--- components/utils/fetchGame.js
+++ components/utils/fetchGame.js
@@ -6,17 +6,27 @@
 import _ from "lodash";
 
 // Fetch one game
-export default function fetchGame(gameDataDirectory) {
+export default function fetchGame() {
   return (
-    fetch(`http://gd2.mlb.com${gameDataDirectory}/boxscore.json`)
+    //fetch(`http://gd2.mlb.com${gameDataDirectory}/boxscore.json`)
+    fetch(
+      "http://gd2.mlb.com/components/game/mlb/year_2016/month_10/day_04/gid_2016_10_04_balmlb_tormlb_1/boxscore.json"
+    )
       // Parse response as JSON
       .then(res => res.json())
       // Cleanup big JSON mess into games
       .then(payload => {
+        // runs, hits and errors
+        const rhe = {
+          runs: payload.data.boxscore.linescore.home_team_runs
+        };
+        // score array with innings
         const scores = payload.data.boxscore.linescore.inning_line_score;
+        // the batters of the home team
         const battersHome = _.find(payload.data.boxscore.batting, {
           team_flag: "home"
         });
+        // the batters of the away team
         const battersAway = _.find(payload.data.boxscore.batting, {
           team_flag: "away"
         });
