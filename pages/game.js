345e03b9c968fce26c715746a78800ef9d006675
Inseo B <yunibaek@hotmail.com>
2017-08-09 22:42:58 -0400

    Page: add batting table to game page

--- pages/game.js
+++ pages/game.js
@@ -1,10 +1,11 @@
-
 import Page from "../components/Page.js";
 import ScoreTable from "../components/ScoreTable.js";
+import BattingTable from "../components/BattingTable.js";
 
 const Game = () => (
   <Page>
     <ScoreTable />
+    <BattingTable />
   </Page>
 );
 
