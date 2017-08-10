7ae62adb90a054671d543dafb71df9a28295c750
Inseo B <yunibaek@hotmail.com>
2017-08-10 03:33:17 -0400

    Add utility function with team names and codes

---
+++ components/utils/teams.js
@@ -0,0 +1,124 @@
+export default function teams() {
+return [
+  {
+    "name": "Pirates",
+    "code": "pit"
+  },
+  {
+    "name": "Reds",
+    "code": "cin"
+  },
+  {
+    "name": "Yankees",
+    "code": "nya"
+  },
+  {
+    "name": "Tigers",
+    "code": "det"
+  },
+  {
+    "name": "Orioles",
+    "code": "bal"
+  },
+  {
+    "name": "Royals",
+    "code": "kca"
+  },
+  {
+    "name": "Red Sox",
+    "code": "bos"
+  },
+  {
+    "name": "Indians",
+    "code": "cle"
+  },
+  {
+    "name": "Marlins",
+    "code": "mia"
+  },
+  {
+    "name": "Nationals",
+    "code": "was"
+  },
+  {
+    "name": "Braves",
+    "code": "atl"
+  },
+  {
+    "name": "Dodgers",
+    "code": "lan"
+  },
+  {
+    "name": "Brewers",
+    "code": "mil"
+  },
+  {
+    "name": "Cardinals",
+    "code": "sln"
+  },
+  {
+    "name": "Cubs",
+    "code": "chn"
+  },
+  {
+    "name": "D-backs",
+    "code": "ari"
+  },
+  {
+    "name": "Rangers",
+    "code": "tex"
+  },
+  {
+    "name": "Mariners",
+    "code": "sea"
+  },
+  {
+    "name": "Astros",
+    "code": "hou"
+  },
+  {
+    "name": "Rays",
+    "code": "tba"
+  },
+  {
+    "name": "White Sox",
+    "code": "cha"
+  },
+  {
+    "name": "Blue Jays",
+    "code": "tor"
+  },
+  {
+    "name": "Rockies",
+    "code": "col"
+  },
+  {
+    "name": "Mets",
+    "code": "nyn"
+  },
+  {
+    "name": "Athletics",
+    "code": "oak"
+  },
+  {
+    "name": "Giants",
+    "code": "sfn"
+  },
+  {
+    "name": "Angels",
+    "code": "ana"
+  },
+  {
+    "name": "Phillies",
+    "code": "phi"
+  },
+  {
+    "name": "Padres",
+    "code": "sdn"
+  },
+  {
+    "name": "Twins",
+    "code": "min"
+  }
+];
+};
