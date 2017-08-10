7568e4e667724e51b498ae776276108de7168f15
Inseo B <yunibaek@hotmail.com>
2017-08-09 21:29:27 -0400

    Score Table: map column on to react-table

--- components/ScoreTable.js
+++ components/ScoreTable.js
@@ -4,14 +4,30 @@ import ReactTable from "react-table";
 
 import fetchGame from "./utils/fetchGame.js";
 
-const COLUMNS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "R", "H", "E"];
+// column labels for table
+const COLUMNS = [
+  "Team Name",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "R",
+  "H",
+  "E"
+];
 
 export default class ScoreTable extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      scores: []
+      scores: [],
+      isLoading: true
     };
   }
 
@@ -19,13 +35,22 @@ export default class ScoreTable extends React.Component {
     // fetch the game scores and batting players
     fetchGame()
       // save into state
-      .then(scores => this.setState({ scores: scores }));
+      .then(scores => this.setState({ scores: scores, isLoading: false }));
   }
 
   render() {
+    if (this.state.isLoading) {
+      return <div>Loading</div>;
+    }
+    // map each label on to the column
     const columns = COLUMNS.map(c => ({ Header: c, accessor: c }));
-    const data = [];
 
-    return <ReactTable data={data} columns={columns} />;
+    const data = this.state.scores.scores;
+    // get the inning scores for home
+    const homeScores = data.map(score => score.home).slice(0, 9);
+    // get the inning scores for away
+    const awayScores = data.map(score => scores.away).slice(0, 9);
+
+    return <ReactTable data={[homeScores, awayScores]} columns={columns} />;
   }
 }
