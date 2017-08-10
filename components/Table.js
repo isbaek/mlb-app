eb7a267dc2e5aa356c4eef4df619f5869bdff377
Inseo B <yunibaek@hotmail.com>
2017-08-10 05:06:40 -0400

    Table: cleanup code

--- components/Table.js
+++ components/Table.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactTable from "react-table";
 
 export default function Table(props) {
-  return <ReactTable
-    showPagination={false}
-    defaultPageSize={props.data.length}
-    sortable={false}
-    resizable={false}
-    {...props}
-  />;
+  return (
+    <ReactTable
+      showPagination={false}
+      defaultPageSize={props.data.length}
+      sortable={false}
+      resizable={false}
+      {...props}
+    />
+  );
 }
