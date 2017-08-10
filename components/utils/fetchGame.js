88a904fffbc12fd701c9c692129f0b2a1bcf8d58
Inseo B <yunibaek@hotmail.com>
2017-08-10 00:26:35 -0400

    fetchGame: now accepts gameDataDirectory argument

--- components/utils/fetchGame.js
+++ components/utils/fetchGame.js
@@ -6,12 +6,10 @@
 import _ from "lodash";
 
 // Fetch one game
-export default function fetchGame() {
+export default function fetchGame(gameDataDirectory) {
   return (
-    //fetch(`http://gd2.mlb.com${gameDataDirectory}/boxscore.json`)
-    fetch(
-      "http://gd2.mlb.com/components/game/mlb/year_2016/month_10/day_04/gid_2016_10_04_balmlb_tormlb_1/boxscore.json"
-    )
+    fetch(`http://gd2.mlb.com${gameDataDirectory}/boxscore.json`)
+    //fetch("http://gd2.mlb.com/components/game/mlb/year_2016/month_10/day_04/gid_2016_10_04_balmlb_tormlb_1/boxscore.json")
       // Parse response as JSON
       .then(res => res.json())
       // Cleanup big JSON mess into games
@@ -22,7 +20,7 @@ export default function fetchGame() {
         // runs, hits and errors for home and away team
         const home = {
           code: boxscore.home_team_code,
-          name: boxscore.home_sname,
+          name: boxscore.home_fname,
           runs: linescore.home_team_runs,
           hits: linescore.home_team_hits,
           errors: linescore.home_team_errors,
@@ -33,7 +31,7 @@ export default function fetchGame() {
         };
         const away = {
           code: boxscore.away_team_code,
-          name: boxscore.away_sname,
+          name: boxscore.away_fname,
           runs: linescore.away_team_runs,
           hits: linescore.away_team_hits,
           errors: linescore.away_team_errors,
