a015a59e1681493f2c226ec55631cadc39446166
Inseo B <yunibaek@hotmail.com>
2017-08-10 08:29:12 -0400

    Error View: add error view css

--- components/ErrorView.js
+++ components/ErrorView.js
@@ -1,16 +1,20 @@
 import React from "react";
+import Link from "next/link";
 
 export default function ErrorView(props) {
-  const msg = props.msg || "Baseball not found";
+  const msg = props.msg;
   return (
-    <div className="container">
-      <img className="ghost-icon" src="/static/ghost.png" />
-      <div className="text-container">
+    <div className="Container">
+      <img src="/static/ghost.png" />
+      <div>
         <h1>
           Error 404
         </h1>
         <h3>{msg}</h3>
         <a href="mailto:contact@inseobaek.com">Notify someone</a>
+        <Link href="/" prefetch>
+          <a>Back to games</a>
+        </Link>
       </div>
     </div>
   );
