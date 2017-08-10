35b85e03347860b29b7cd577ceca3fd69115c9b2
Inseo B <yunibaek@hotmail.com>
2017-08-10 04:01:44 -0400

    Updates

---
+++ _next/2d87176f-265e-441c-ba46-22a3a9638a1c/page/about/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/about', function() {
+            var comp = module.exports=webpackJsonp([7],{469:function(e,t,n){e.exports=n(470)},470:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=n(2),l=function(e){return e&&e.__esModule?e:{default:e}}(u),o=function(){return l.default.createElement("div",null,l.default.createElement("p",null,"Hello Inseo Baek"))};t.default=o}},[469]);
+            return { page: comp.default }
+          })
+        
