b61307aa9e3c407007a93c176d77d4bb656844e2
Inseo B <yunibaek@hotmail.com>
2017-08-09 23:22:25 -0400

    Loading View: add a loading before checking data

---
+++ components/LoadingView.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/loadingview.css";
+
+export default function LoadingPage() {
+  return (
+    <div className="container">
+      <div className="loader" />
+    </div>
+  );
+}
