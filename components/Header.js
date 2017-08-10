98e4c560d22d3e48c04fea97042dc0652aec3a18
Inseo B <yunibaek@hotmail.com>
2017-08-10 02:38:16 -0400

    Header: link back to homepage

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 function Header() {
-    return (
-    <div className="Header">
+  return (
+    <div className="Header" prefetch>
+      <Link href="/">
         <img className="HeaderLogo" src="/static/mlb_logo_short.png" />
+      </Link>
     </div>
-    );
+  );
 }
 
 export default Header;
